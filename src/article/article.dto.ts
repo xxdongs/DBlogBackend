1abc0950f303f08b6eb486788f0747bbb9da885a
edgar <doforce@126.com>
2020-11-23 10:19:53 +0800

    remove import

--- src/article/article.dto.ts
+++ src/article/article.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, ArrayMinSize, ArrayNotEmpty } from "class-validator"
+import { IsNotEmpty, ArrayMinSize } from "class-validator"
 import { ApiProperty } from "@nestjs/swagger"
 
 export class ArticleCreateDto {
