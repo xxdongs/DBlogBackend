cc1775c57e517170eee20415649b9328e24dde06
谢小东 <doforce@126.com>
2020-11-26 00:29:35 +0800

    set mysql timezone to +8:00

--- mysql.sh
+++ mysql.sh
@@ -5,4 +5,4 @@ docker run --name blog -p 3306:3306 \
 -e MYSQL_DATABASE=blog \
 -e MYSQL_USER=edgar \
 -e MYSQL_PASSWORD=1234 \
--d mysql:8 --default-authentication-plugin=mysql_native_password 
+-d mysql:8 --default-authentication-plugin=mysql_native_password --default-time_zone=+8:00
